e4b780f6d9ac467f9862f9646abfbb38856f417b
unknown <elias.braem@students.fhnw.ch>
2023-07-06 15:59:06 +0200

    hashmap to handle state

---
+++ src/components/Form/stepTwo/Registrierung.js
@@ -0,0 +1,43 @@
+import React, { useContext, useEffect } from "react";
+import { useState } from 'react';
+import Accordion from "../../Accordion";
+import { DataContext } from "./../../../DataContext";
+
+
+
+const Registrierung = ({update}) => {
+  const [data, setData] = useState(useContext(DataContext));
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    if (data.technischAufbewahrung != 0) {setStatus(true);     }
+  }, [data]);
+
+  useEffect(() => {
+    if (status) {update("registrierung")(true);}
+    else {update("registrierung")(false)}
+    }, [status]);
+
+  const technischeDaten = <><div className="accordion-input">
+    <label>Wie lange werden die Daten aufbewahrt?</label>
+    <div className="input-row-group">
+    <input type="number"
+    min={0}
+    value={data.technischAufbewahrung}
+    onChange={ (e) => setData({ ...data, technischAufbewahrung: e.target.value })} />
+
+    <p>Monate</p>
+    </div>
+  </div>
+  <div className="accordion-input">
+  <label>Bieten Sie ein öffentliches Wlan an?</label>
+  <input type="checkbox"></input>
+</div>
+</>;
+
+  return (
+      <Accordion title="Registrierungsdaten" content={technischeDaten} status={status? "green" :"red"} open={true} />
+    )
+}
+
+export default Registrierung;
